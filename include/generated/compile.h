2bde101e5b96e9f2bbfbf061a8ebf4b2113f0fc7
jeybee <jethrob@hotmail.com>
2023-03-10 01:39:19 +0000

    clean up

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 142 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#142 SMP PREEMPT Thu Mar 9 20:26:08 GMT 2023"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "usr-ProLiant-DL360-G7"
-#define LINUX_COMPILER "gcc version 4.9.3 20150529 (prerelease) (15:4.9.3+svn231177-1) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.113-ga28197b-dirty"
